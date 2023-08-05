ca0a8d844ecd6df36c8e6445e970942f1b0e36f2
esohi <esohi@proton.me>
2023-08-05 16:27:32 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// Challenge 1
+let i = 1;
+while (i <= 20) {
+    if (i % 2 !==0) console.log(i);
+    i++;
+}
+
+// Challenge 2
+let x = 100
+for (x = 100; x >= 0; x--) {
+    if (x % 3 === 0) console.log(x);
+}
+
+// Challenge 3
+let y = 4
+for (y=4; y >=-3.5; y-=1.5) {
+    console.log(y);
+}
+
+//  Challenge 4
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+    console.log(sum);
+}
+
+// Challenge 5
+let product = 1;
+for (let i = 1; i <= 12; i++) {
+    product *= i;
+    console.log(product);
+}
